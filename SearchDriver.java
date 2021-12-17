7aea29995869ff5b9a167b5e593a9bf728eb15e1
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-16 19:12:33 -0500

    Update SearchDriver.java

--- SearchDriver.java
+++ SearchDriver.java
@@ -4,10 +4,23 @@ APCS pd6
 L03 - Binary vs Linear
 2021-12-16
 time spent:  0.5 hours
+
 DISCO
 -
 QCC
 -
  *****************************************************/
  
- 
+ public class SearchDriver {
+  
+  public static void main(String[] args) {
+   long a = currentTimeMillis();
+   print("5");
+   long b = currentTimeMillis();
+   print(b-a); 
+   
+   
+   
+  } // end main
+  
+ }
