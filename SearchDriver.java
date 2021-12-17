975ecdd9d2518b325631659325d08fde00b73554
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-16 19:15:52 -0500

    Update SearchDriver.java

--- SearchDriver.java
+++ SearchDriver.java
@@ -16,6 +16,17 @@ QCC
   public static void main(String[] args) {
    long a = System.currentTimeMillis();
    System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
+   System.out.println("5");
    long b = System.currentTimeMillis();
    System.out.println(b-a); 
    
