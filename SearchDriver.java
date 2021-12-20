d98b2e59474a0433338f4586a2a11c3b52dc8186
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-20 17:51:43 -0500

    Update SearchDriver.java

--- SearchDriver.java
+++ SearchDriver.java
@@ -7,8 +7,10 @@ time spent:  0.5 hours
 
 DISCO
 - currentTimeMillis() gives the current time in miliseconds
+- We had to increase repetition to increase the amount of time that the code took to run. 
 QCC
 - Are there other time stamp methods that give time in other units of time?
+- What run time should we be getting?
  *****************************************************/
 
 public class SearchDriver{
@@ -28,9 +30,9 @@ public class SearchDriver{
       return timePassed; // returns total time for search
     }
 
-    public static long indexTestBAll(Comparable[] intArray, int itNum){
+    public static long indexTestBAll(Comparable[] intArray){
       long start = System.currentTimeMillis();
-      for (int i = 0; i < itNum; i ++) {
+      for (int i = 0; i < intArray.length; i ++) {
         BinSearch.binSearch(intArray, intArray[i]);
       } // Binary search on every element of the list
       long end = System.currentTimeMillis();
@@ -38,9 +40,9 @@ public class SearchDriver{
       return timePassed; // returns total time for search
     }
 
-    public static long indexTestIAll(Comparable[] intArray, int itNum){
+    public static long indexTestIAll(Comparable[] intArray){
       long start = System.currentTimeMillis();
-      for (int i = 0; i < itNum; i ++) {
+      for (int i = 0; i < intArray.length; i ++) {
         LinSearch.linSearch(intArray, intArray[i]);
       } // Binary search on every element of the list
       long end = System.currentTimeMillis();
@@ -49,38 +51,50 @@ public class SearchDriver{
     }
 
     public static void main(String[] args){
+      // Initializing arrays
       Comparable[] test = new Integer[10000000];
       Comparable[] test2 = new Integer[5000000];
-      for (int i = 0; i < test.length; i++){ // populates test
-        test[i] = i*2;
+      Comparable[] test3 = new Integer[10000];
+      Comparable[] test4 = new Integer[20000];
+      Comparable[] test5 = new Integer[100000];
+      // Populating arrays
+      for (int i = 0; i < test.length; i++){ 
+        test[i] = i*1;
       }
-      for (int i = 0; i < test2.length; i++){ // populates test
-        test2[i] = i*2;
+      for (int i = 0; i < test2.length; i++){ 
+        test2[i] = i*1;
       }
+      for (int i = 0; i < test.length; i++){ 
+        test3[i] = i*1;
+      }
+      for (int i = 0; i < test2.length; i++){ 
+        test4[i] = i*1;
+      }
+      for (int i = 0; i < test2.length; i++){ 
+        test5[i] = i*1;
+      }
+      // Testing on the Arrays
       System.out.println("testing for speed of linear and binary search on big array for small index:");
       System.out.println("total time(ms) for linear search: \t" + indexTestI(test, 5000));
       System.out.println("total time(ms) for binary search: \t" + indexTestB(test, 5000));
       System.out.println("testing for speed of linear and binary search on big array for big index:");
       System.out.println("total time(ms) for linear search: \t" + indexTestI(test, 9999999));
       System.out.println("total time(ms) for binary search: \t" + indexTestB(test, 9999999));
-      System.out.println("testing for speed of linear and binary search on big array for index in the middle:");
-      System.out.println("total time(ms) for linear search: \t" + indexTestI(test, 4999999));
-      System.out.println("total time(ms) for binary search: \t" + indexTestB(test, 4999999));
       System.out.println("testing for speed of linear and binary search on small array for small index:");
       System.out.println("total time(ms) for linear search: \t" + indexTestI(test2, 5000));
       System.out.println("total time(ms) for binary search: \t" + indexTestB(test2, 5000));
       System.out.println("testing for speed of linear and binary search on small array for big index:");
       System.out.println("total time(ms) for linear search: \t" + indexTestI(test2, 4999999));
       System.out.println("total time(ms) for binary search: \t" + indexTestB(test2, 4999999));
-      System.out.println("testing for speed of linear and binary search on small array for index in the middle:");
-      System.out.println("total time(ms) for linear search: \t" + indexTestI(test2, 2499999));
-      System.out.println("total time(ms) for binary search: \t" + indexTestB(test2, 2499999));
       System.out.println("testing for speed of linear and binary search on array for 10000 index:");
-      System.out.println("total time(ms) for linear search: \t" + indexTestIAll(test, 10000));
-      System.out.println("total time(ms) for binary search: \t" + indexTestBAll(test, 10000));
+      System.out.println("total time(ms) for linear search: \t" + indexTestIAll(test3));
+      System.out.println("total time(ms) for binary search: \t" + indexTestBAll(test3));
       System.out.println("testing for speed of linear and binary search on array for 20000 index:");
-      System.out.println("total time(ms) for linear search: \t" + indexTestIAll(test, 20000));
-      System.out.println("total time(ms) for binary search: \t" + indexTestBAll(test, 20000));
+      System.out.println("total time(ms) for linear search: \t" + indexTestIAll(test4));
+      System.out.println("total time(ms) for binary search: \t" + indexTestBAll(test4));
+      System.out.println("testing for speed of linear and binary search on array for 100000 index:");
+      System.out.println("total time(ms) for linear search: \t" + indexTestIAll(test5));
+      System.out.println("total time(ms) for binary search: \t" + indexTestBAll(test5));
     }
 
 }
